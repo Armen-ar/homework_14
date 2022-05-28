d5ec998899adf24eb36af29e5b1b94b1369679f9
Armen-ar <armen.aruts1967@gmail.com>
2022-05-28 23:27:55 +0300

    5_task_1

---  movie_search/app.py
+++  movie_search/app.py
@@ -1,7 +1,7 @@
 import logging
 
 import logger as logger
-from flask import Flask
+from flask import Flask, jsonify
 from utils import search_by_title, search_by_range_of_years, search_by_rating, search_by_genre
 
 logger.create_logger()
@@ -10,6 +10,7 @@ logger = logging.getLogger("basic")
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+app.config['JSON_SORT_KEYS'] = False
 
 
 @app.route('/movie/<title>')
@@ -26,7 +27,7 @@ def search_movie_by_title(title):
 def search_movie_by_range_of_years(year_1, year_2):
     logger.debug("Запрошен фильм по дате выпуска в прокат")
     try:
-        movie_title_release_year = search_by_range_of_years(year_1, year_2)
+        movie_title_release_year = jsonify(search_by_range_of_years(year_1, year_2))
         return movie_title_release_year
     except:
         return "Ошибка поиска фильма по дате выпуска в прокат"
@@ -36,13 +37,7 @@ def search_movie_by_range_of_years(year_1, year_2):
 def search_movie_by_rating(rating):
     logger.debug("Запрошен фильм по рейтингу")
     try:
-        if rating == 'children':
-            movie_by_rating = search_by_rating('(\'G\')')
-        elif rating == 'family':
-            movie_by_rating = search_by_rating(('G', 'PG', 'PG-13'))
-        elif rating == 'adult':
-            movie_by_rating = search_by_rating(('R', 'NC-17'))
-
+        movie_by_rating = jsonify(search_by_rating(rating))
         return movie_by_rating
     except:
         return "Ошибка поиска фильма по рейтингу"
@@ -52,7 +47,7 @@ def search_movie_by_rating(rating):
 def search_movie_by_genre(genre):
     logger.debug("Запрошен фильм по жанру")
     try:
-        movie_by_genre = search_by_genre(genre)
+        movie_by_genre = jsonify(search_by_genre(genre))
         return movie_by_genre
     except:
         return "Ошибка поиска фильма по жанру"
@@ -60,8 +55,3 @@ def search_movie_by_genre(genre):
 
 if __name__ == "__main__":
     app.run(debug=True, port=2910)
-
-# genre: Documentaries, Music & Musicals, International TV Shows, Reality TV,
-# Documentaries, International Movies, Music & Musicals, Dramas, International Movies, Music & Musicals
-# Children & Family Movies, Comedies, International TV Shows, Spanish-Language TV Shows, Stand-Up Comedy & Talk Shows
-# Comedies, Horror Movies, Kids TV, Korean TV Shows, TV Comedies
---  movie_search/utils.py
+++  movie_search/utils.py
@@ -1,135 +1,153 @@
-import json
 import sqlite3
+from collections import Counter
+
+from flask import jsonify
+
+
+def execute_query(query):
+    with sqlite3.connect("../netflix.db") as connection:
+        cur = connection.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+        return result
 
 
 def search_by_title(title):
-    with sqlite3.connect("../netflix.db") as connection:
-        cursor = connection.cursor()
-        query = (f"""SELECT title, country, release_year, listed_in, description
-                FROM netflix 
-                WHERE type = 'Movie' AND title LIKE '{title}%'
-                ORDER BY release_year DESC LIMIT 1
-                """
-                 )
-        cursor.execute(query)
-        title_movie = cursor.fetchall()[0]
+    query = f"""SELECT title, country, release_year, listed_in, description
+    FROM netflix 
+    WHERE type = 'Movie' AND title LIKE '{title}%'
+    ORDER BY release_year DESC LIMIT 1
+    """
+
+    title_movie = execute_query(query)
 
     movie_title = {
-        "title": title_movie[0],
-        "country": title_movie[1],
-        "release_year": title_movie[2],
-        "genre": title_movie[3],
-        "description": title_movie[4]
+        "title": title_movie[0][0],
+        "country": title_movie[0][1],
+        "release_year": title_movie[0][2],
+        "genre": title_movie[0][3],
+        "description": title_movie[0][4]
     }
 
-    json_movie_title = json.dumps(movie_title)
+    json_movie_title = jsonify(movie_title)
 
     return json_movie_title
 
 
 def search_by_range_of_years(year_1, year_2):
-    with sqlite3.connect("../netflix.db") as connection:
-        cursor = connection.cursor()
-        query = (f"""SELECT title, release_year
-                FROM netflix
-                WHERE release_year BETWEEN '{year_1}' AND '{year_2}'
-                ORDER BY release_year
-                LIMIT 100
-                """
-                 )
-        cursor.execute(query)
-        range_of_years = cursor.fetchall()
+    query = f"""SELECT title, release_year
+    FROM netflix
+    WHERE release_year BETWEEN '{year_1}' AND '{year_2}'
+    ORDER BY release_year
+    LIMIT 100
+    """
 
-        movie_title_release_year = []
-        for i in range_of_years:
-            movie_title = {
-                "title": i[0],
-                "release_year": i[1],
-            }
-            movie_title_release_year.append(movie_title)
+    range_of_years = execute_query(query)
 
-        json_movie_by_range_of_years = json.dumps(movie_title_release_year)
+    movie_title_release_year = []
+    for movie in range_of_years:
+        movie_title = {
+            "title": movie[0],
+            "release_year": movie[1],
+        }
 
-        return json_movie_by_range_of_years
+        movie_title_release_year.append(movie_title)
+
+    return movie_title_release_year
 
 
 def search_by_rating(rating):
-    with sqlite3.connect("../netflix.db") as connection:
-        cursor = connection.cursor()
-        query = (f"""SELECT title, rating, description
-                FROM netflix
-                WHERE rating IN {rating}
-                LIMIT 100
-                """
-                 )
-        cursor.execute(query)
-        by_rating = cursor.fetchall()
+    parameters = {
+        "children": "'G'",
+        "family": "'G', 'PG', 'PG-13'",
+        "adult": "'R', 'NC-17'"
+    }
+    if rating not in parameters:
+        return "Введённой группы не существует"
 
-        movie_by_rating = []
-        for i in by_rating:
-            movie_one_by_rating = {
-                "title": i[0],
-                "rating": i[1],
-                "description": i[2]
-            }
-            movie_by_rating.append(movie_one_by_rating)
+    query = f"""SELECT title, rating, description
+    FROM netflix
+    WHERE rating IN ({parameters[rating]})
+    """
 
-        json_movie_by_rating = json.dumps(movie_by_rating)
+    movie_by_rating_all = execute_query(query)
 
-        return json_movie_by_rating
+    movie_by_rating = []
+    for movie in movie_by_rating_all:
+        movie_one_by_rating = {
+            "title": movie[0],
+            "rating": movie[1],
+            "description": movie[2]
+        }
+        movie_by_rating.append(movie_one_by_rating)
+
+    return movie_by_rating
 
 
 def search_by_genre(genre):
-    with sqlite3.connect("../netflix.db") as connection:
-        cursor = connection.cursor()
-        query = (f"""SELECT title, description
-                FROM netflix
-                WHERE listed_in LIKE '%{genre}%'
-                ORDER BY release_year DESC
-                LIMIT 10
-                """
-                 )
-        cursor.execute(query)
-        by_genre = cursor.fetchall()
+    query = f"""SELECT title, description
+    FROM netflix
+    WHERE listed_in LIKE '%{genre}%'
+    ORDER BY release_year DESC
+    LIMIT 10
+    """
 
-        movie_by_genre = []
-        for i in by_genre:
-            movie_one_by_genre = {
-                "title": i[0],
-                "description": i[1]
-            }
-            movie_by_genre.append(movie_one_by_genre)
+    by_genre = execute_query(query)
 
-        json_movie_by_genre = json.dumps(movie_by_genre)
+    movie_by_genre = []
+    for movie in by_genre:
+        movie_one_by_genre = {
+            "title": movie[0],
+            "description": movie[1]
+        }
+        movie_by_genre.append(movie_one_by_genre)
 
-        return json_movie_by_genre
+    return movie_by_genre
 
 
 def search_by_actors(actor_1, actor_2):
-    with sqlite3.connect("../netflix.db") as connection:
-        cursor = connection.cursor()
-        query = (f"""SELECT netflix.cast
-                FROM netflix
-                WHERE netflix.cast LIKE '%{actor_1}%' AND netflix.cast LIKE '%{actor_2}%'
-                """
-                 )
-        cursor.execute(query)
-        movie_by_actors = cursor.fetchall()
+    query = f"""SELECT `cast`
+    FROM netflix
+    WHERE `cast` LIKE '%{actor_1}%' AND `cast` LIKE '%{actor_2}%'
+    """
 
-        movie_by_actors_all = []
-        for i in movie_by_actors:
-            movie_by_actor = {
-                "cast": i[0]
-            }
-            movie_by_actors_all.append(movie_by_actor)
+    movie_by_actors_all = execute_query(query)
 
-        json_movie_by_actors_all = json.dumps(movie_by_actors_all)
+    movie_by_actors = []
+    for cast in movie_by_actors_all:
+        movie_by_actors.extend(cast[0].split(', '))
+    counter = Counter(movie_by_actors)
 
-    return json_movie_by_actors_all
+    movie_by_actor = []
+    for actor, count in counter.items():
+        if actor not in [actor_1, actor_2] and count > 2:
+            movie_by_actor.append(actor)
+
+    return movie_by_actor
 
 
-# print(search_by_title('Am'))
-# print(search_by_range_of_years(2019, 2020))
-# print(search_by_genre('Documentaries'))
 # print(search_by_actors('Rose McIver', 'Ben Lamb'))
 # print(search_by_actors('Jack Black', 'Dustin Hoffman'))
+
+
+def search_movie_by_param(type_movie, release_year, genre):
+    query = f"""SELECT title, description
+    FROM netflix
+    WHERE type = '{type_movie}' AND release_year = {release_year} AND listed_in LIKE '%{genre}%'
+    """
+
+    by_param = execute_query(query)
+
+    movie_by_param = []
+    for movie in by_param:
+        movie_one_by_param = {
+            "title": movie[0],
+            "description": movie[1]
+        }
+
+        movie_by_param.append(movie_one_by_param)
+
+    return movie_by_param
+
+
+# print(search_movie_by_param('Movie', 2017, 'Documentaries'))
